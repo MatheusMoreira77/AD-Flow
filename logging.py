0cb0b490c29b8904f4bb2195cc8cc24d96f673fc
MatheusMoreira77 <matheussmoreira7@gmail.com>
2025-08-12 00:16:41 -0300

    Cabeçalho

---
+++ logging.py
@@ -0,0 +1,10 @@
+#
+#=========================================
+#AD-Flow - Automação Active Directory
+#Desenvolvido por: Matheus Santos Moreira
+#Data: 11/08/2025
+#Contato: https://www.linkedin.com/in/matheus-santos-moreira-0a0812261/
+#Versão: 1.0
+#Descrição: Script para automação de criação e gerenciamento de usuários no Active Directory via terminal.
+#=========================================
+#
